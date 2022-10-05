20b71a2a3306fbf7a0065d4b49e4876842aacc7d
Guilherme Silva <guilherme.silva@yazo.com.br>
2022-10-04 22:10:25 -0300

    atualizada base de carros

--- StitchStoryMaker/story.js
+++ StitchStoryMaker/story.js
@@ -2,82 +2,16 @@
 $( document ).ready(function() {
 
 	var carros;
-	
-	$('#myCanvas').drawImage({
-		source: 'carDataBg.png',
-		x: (1080/2), y: (1080/2)
-	});
-	
-	$('#soldPostCanvas').addLayer({
+
+	$('#StoryCanvas').addLayer({
 		type: 'image',
-		source: 'soldOverlayUp.png',
-		x: (1080/2), y: 98
+		source: 'StoryOverlayTop.png',
+		x: (1080/2), y: 195
 	}).addLayer({
 		type: 'image',
-		source: 'soldOverlayBottom.png',
-		x: (1080/2), y: 915
+		source: 'StoryOverlayBottom.png',
+		x: (1080/2), y:1570
 	}).drawLayers();
-	
-	$.ajax({
-	  url: "carData.json",
-	  type: 'GET',
-	  dataType: 'json'
-	}).done(function(e) {
-	
-		for (var i = 0; i <= e.length - 1; i++) {
-			$('#carros').append("<option value='"+i+"'>"+e[i].carName+"</option>")
-			// console.log(e[i].carName);
-		}
-	
-		$("#carros").on("change", function(evt){
-			// console.log(e[$(this).val()].carName);
-			$('#carName').text(e[$(this).val()].carName);
-			$('#milhagem').text(e[$(this).val()].Milhagem);
-			$('#motor').text(e[$(this).val()].Motor);
-			$('#sist').text(e[$(this).val()].SistemaTransmissao);
-			$('#trim').text(e[$(this).val()].Trim);
-			$('#mpg').text(e[$(this).val()].MPG);
-			$('#transm').text(e[$(this).val()].Transmissao);
-			$('#price').text(e[$(this).val()].carPrice);
-	
-	
-			var ano = e[$(this).val()].carName.slice(0,4);
-	
-	
-	
-			$('#myCanvas').drawImage({
-			  source: 'carDataBg.png',
-			  x: (1080/2), y: (1080/2)
-			});
-	
-			$('#myCanvas').drawText({
-			  fillStyle: '#F0AF39',
-			  x: (1080/2), y: 230,
-			  fontSize: 80,
-			  fontFamily: 'Bebas Neue, cursive',
-			  text: (e[$(this).val()].carName),
-			  maxWidth: 800
-			});
-	
-			$('#myCanvas').drawText({
-			  fillStyle: '#F0AF39',
-			  x: (1080/2), y: 900,
-			  fontSize: 150,
-			  fontFamily: 'Bebas Neue, cursive',
-			  text: (e[$(this).val()].carPrice)
-			});
-		})
-	});
-	
-	$('#tabSelector button').on('click', function(e){
-		if ($(this).attr('class') != 'active'){
-			$('#tabSelector button').removeClass('active');
-			$(this).addClass('active');
-			$('.tabContent').addClass('hidden');
-			var targetTab = $('#'+$(this).data('target'));
-			targetTab.removeClass('hidden');
-		}
-	})
 	
 
 
@@ -147,6 +81,51 @@ $('#removerImagem').on('click', function(){
 		x: (1080/2), y: 915
 	}).drawLayers();
 	$('#soldImageUpload').removeClass('hidden');
+})
+
+
+$('#storyImageUpload input').on('change', function(e){
+	$('#storyImageUpload').addClass('hidden');
+	$('#baixarImagem').removeClass('hidden');
+	var storyImage = new Image();
+	storyImage.src = URL.createObjectURL(e.target.files[0]);
+	var scale = 0.8
+	$('#StoryCanvas').addLayer({
+		type: 'image',
+		name: 'storyImage',
+	  	source: storyImage,  
+	  	x: (1080/2), y: 1080/2,
+		scale:scale,
+		draggable: true
+	}).addLayer({
+		type: 'image',
+		source: 'storyOverlayUp.png',
+		x: (1080/2), y: 98
+	}).addLayer({
+		type: 'image',
+		source: 'storyOverlayBottom.png',
+		x: (1080/2), y: 915
+	}).drawLayers();
+
+	// $('.resizeButton').on('click',function(e){
+	// 	e.preventDefault();
+	// 	if($(this).data('resize') == 'Up'){
+	// 		$('#storyPostCanvas').setLayer('storyImage',{
+	// 			scale:scale+0.2
+	// 		}).drawLayers();
+	// 		scale=scale+0.2
+	// 	}else if($(this).data('resize') == 'Down'){
+	// 		$('#storyPostCanvas').setLayer('storyImage',{
+	// 			scale:scale-0.2
+	// 		}).drawLayers();
+	// 		scale=scale-0.2
+	// 	}
+	// })
+
+	// $('#baixarImagem').on('click', function(e){
+	// 	$(this).attr('href', $('#storyPostCanvas').getCanvasImage('png'));
+	// })
+
 });
 
 
