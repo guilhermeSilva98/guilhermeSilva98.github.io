5a14f0e3d4e4404571c895071098539f65989a98
Guilherme Silva <guilherme.silva@yazo.com.br>
2022-12-07 23:31:48 -0300

    Story Maker implementado

--- StitchStoryMaker/story.js
+++ StitchStoryMaker/story.js
@@ -1,132 +1,170 @@
-
 $( document ).ready(function() {
 
-	var carros;
+	var storyImage;
+	var scale = 1;
+	var nomeCarro;
+	var anoCarro;
+	var entrada;
+	var parcela;
+	var selo;
+	var position = 1920/2.5;
+	var selo = new Image();
 
-	$('#StoryCanvas').addLayer({
+	
+
+$('.selo').on('change', function(e){
+	selo.src = 'badges/'+$(this).data('selo')+'.png';
+	console.log(selo);
+})
+
+$('#criarStory').on('click', function(e){
+	e.preventDefault();
+	$('#adStoryCanvas').clearCanvas();
+	$('#baixarImagem').removeClass('hidden');
+	$('#baixarImagem').on('click', function(e){
+		$(this).attr('href', $('#adStoryCanvas').getCanvasImage('png'));
+	})
+
+	var infoCarro = $('#fld-nomeCarro').val();
+	anoCarro = infoCarro.slice(0,4);
+	nomeCarro = infoCarro.slice(5);
+
+	entrada = '$'+$('#fld-entrada').val().slice(0,1)+'.'+$('#fld-entrada').val().slice(1)+',00';
+	parcela = $('#fld-parcela').val();
+
+	$('#adStoryCanvas').addLayer({
+		
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: (1080/2), y: 1770,
+		fontSize: 80,
+		fontFamily: 'Montserrat',
+		text: '/MÊS',
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: ((1080/3)+50), y: 1630,
+		fontSize: 300,
+		fontFamily: 'Bebas Neue, cursive',
+		text: parcela,
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: ((1080/4)-80), y: 1560,
+		fontSize: 100,
+		fontFamily: 'Bebas Neue, cursive',
+		text: '$',
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: (1080/4), y: 1440,
+		fontSize: 100,
+		fontFamily: 'Bebas Neue, cursive',
+		text: entrada,
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#6d6c6a',
+		x: (1080/2.4), y: 1440,
+		fontSize: 200,
+		fontFamily: 'Bebas Neue, cursive',
+		text: '+',
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: (1080/4), y: 1350,
+		fontSize: 40,
+		fontFamily: 'Montserrat, cursive',
+		text: 'ENTRADA DE',
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: (1080/2), y: 220,
+		fontSize: 40,
+		fontFamily: 'Montserrat, cursive',
+		text: anoCarro,
+		maxWidth: 800
+	}).drawText({
+		layer: true,
+		index: 2,
+		fillStyle: '#F0AF39',
+		x: (1080/2), y: 290,
+		fontSize: 80,
+		fontFamily: 'Bebas Neue, cursive',
+		text: nomeCarro,
+		maxWidth: 800
+	}).addLayer({
 		type: 'image',
+		index:1,
+		source: selo,
+		x: (1080/1.4), y: 1350
+	}).addLayer({
+		type: 'image',
+		index:1,
 		source: 'StoryOverlayTop.png',
 		x: (1080/2), y: 195
 	}).addLayer({
 		type: 'image',
+		index:1,
 		source: 'StoryOverlayBottom.png',
-		x: (1080/2), y:1570
-	}).drawLayers();
-	
-
-
-$('#soldImageUpload input').on('change', function(e){
-	$('#soldImageUpload').addClass('hidden');
-	$('#baixarImagem').removeClass('hidden');
-	var soldImage = new Image();
-	soldImage.src = URL.createObjectURL(e.target.files[0]);
-	var scale = 0.8
-	$('#soldPostCanvas').addLayer({
-		type: 'image',
-		name: 'soldImage',
-	  	source: soldImage,  
-	  	x: (1080/2), y: 1080/2,
-		scale:scale,
-		draggable: true
+		x: (1080/2), y:1575
 	}).addLayer({
-		type: 'image',
-		source: 'soldOverlayUp.png',
-		x: (1080/2), y: 98
-	}).addLayer({
-		type: 'image',
-		source: 'soldOverlayBottom.png',
-		x: (1080/2), y: 915
-	}).drawLayers();
-
-	$('.resizeButton').on('click',function(e){
-		e.preventDefault();
-		if($(this).data('resize') == 'Up'){
-			$('#soldPostCanvas').setLayer('soldImage',{
-				scale:scale+0.2
-			}).drawLayers();
-			scale=scale+0.2
-		}else if($(this).data('resize') == 'Down'){
-			$('#soldPostCanvas').setLayer('soldImage',{
-				scale:scale-0.2
-			}).drawLayers();
-			scale=scale-0.2
-		}
-	})
-
-	$('#baixarImagem').on('click', function(e){
-		$(this).attr('href', $('#soldPostCanvas').getCanvasImage('png'));
-	})
-
-	// soldImage.onload = function(){
-	// 	// var image = $('#soldPostCanvas').getCanvasImage('png');
-	// 	// $('#baixarImagem').attr('download', 'sold.png');
-	// 	// $('#baixarImagem').attr('href', image);
-		
-		
-	// }
-
-});
-
-
-$('#removerImagem').on('click', function(){
-	$('#baixarImagem').addClass('hidden');
-	$('#soldPostCanvas').removeLayer('soldImage').drawLayers();
-	$('#soldPostCanvas').addLayer({
-		type: 'image',
-		source: 'soldOverlayUp.png',
-		x: (1080/2), y: 98
-	}).addLayer({
-		type: 'image',
-		source: 'soldOverlayBottom.png',
-		x: (1080/2), y: 915
-	}).drawLayers();
-	$('#soldImageUpload').removeClass('hidden');
-})
-
-
-$('#storyImageUpload input').on('change', function(e){
-	$('#storyImageUpload').addClass('hidden');
-	$('#baixarImagem').removeClass('hidden');
-	var storyImage = new Image();
-	storyImage.src = URL.createObjectURL(e.target.files[0]);
-	var scale = 0.8
-	$('#StoryCanvas').addLayer({
 		type: 'image',
 		name: 'storyImage',
 	  	source: storyImage,  
-	  	x: (1080/2), y: 1080/2,
+	  	x: (1080/2), y: position,
 		scale:scale,
-		draggable: true
-	}).addLayer({
-		type: 'image',
-		source: 'storyOverlayUp.png',
-		x: (1080/2), y: 98
-	}).addLayer({
-		type: 'image',
-		source: 'storyOverlayBottom.png',
-		x: (1080/2), y: 915
-	}).drawLayers();
+		draggable: true,
+		index:0,
+	});
 
-	// $('.resizeButton').on('click',function(e){
-	// 	e.preventDefault();
-	// 	if($(this).data('resize') == 'Up'){
-	// 		$('#storyPostCanvas').setLayer('storyImage',{
-	// 			scale:scale+0.2
-	// 		}).drawLayers();
-	// 		scale=scale+0.2
-	// 	}else if($(this).data('resize') == 'Down'){
-	// 		$('#storyPostCanvas').setLayer('storyImage',{
-	// 			scale:scale-0.2
-	// 		}).drawLayers();
-	// 		scale=scale-0.2
-	// 	}
-	// })
-
-	// $('#baixarImagem').on('click', function(e){
-	// 	$(this).attr('href', $('#storyPostCanvas').getCanvasImage('png'));
-	// })
-
+	$('#adStoryCanvas').drawLayers();
 });
+
+
+$('#fld-imagem').on('change', function(e){
+	storyImage = new Image();
+	storyImage.src = URL.createObjectURL(e.target.files[0]);
+});
+
+
+$('.adjustBtn').on('click',function(e){
+	e.preventDefault();
+	if($(this).data('resize') == 'Up'){
+		$('#adStoryCanvas').setLayer('storyImage',{
+			scale:scale+0.2
+		}).drawLayers();
+		scale=scale+0.2
+	}else if($(this).data('resize') == 'Down'){
+		$('#adStoryCanvas').setLayer('storyImage',{
+			scale:scale-0.2
+		}).drawLayers();
+		scale=scale-0.2
+	}else if($(this).data('nudge') == 'Up'){
+		$('#adStoryCanvas').setLayer('storyImage',{
+			y:position-10
+		}).drawLayers();
+		position = position-10;
+	}else if($(this).data('nudge') == 'Down'){
+		$('#adStoryCanvas').setLayer('storyImage',{
+			y:position+10
+		}).drawLayers();
+		position = position+10;
+	}
+})
 
 
 
