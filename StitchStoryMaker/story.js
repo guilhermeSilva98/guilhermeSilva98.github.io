da98e9096fd580badf53d31fb4bdc816d5e3ddf4
Guilherme Silva <guilherme.silva@yazo.com.br>
2023-02-14 09:57:26 -0300

    adicionado cash price

--- StitchStoryMaker/story.js
+++ StitchStoryMaker/story.js
@@ -7,9 +7,12 @@ $( document ).ready(function() {
 	var anoCarro;
 	var entrada;
 	var parcela;
+	var cashPrice;
 	var selo;
 	var position = 1920/2.3;
 	var selo = new Image();
+
+	
 
 	
 
@@ -26,116 +29,216 @@ $('#criarStory').on('click', function(e){
 		$(this).attr('href', $('#adStoryCanvas').getCanvasImage('png'));
 	})
 
+	const text = $('#fld-texto').val();
+	const entryRegex = /Entrada\s\$(\d+)/;
+	const monthlyRegex = /\+\s\$(\d+)\s/;
+	const singleRegex = /\$(\d+)\s/;
+
+	const entryMatch = text.match(entryRegex);
+	const monthlyMatch = text.match(monthlyRegex);
+	const singleMatch = text.match(singleRegex);
+
+	let entryPrice = 0;
+	let monthlyPrice = 0;
+	let singlePrice = 0;
+
+	if (entryMatch && monthlyMatch) {
+		entryPrice = parseInt(entryMatch[1]);
+		monthlyPrice = parseInt(monthlyMatch[1]);
+	} else if (singleMatch) {
+		singlePrice = parseInt(singleMatch[1]);
+	}
+
+	
+
 	baseText = $('#fld-texto').val().split(/(?:\r?\n)+/);
-
-	var infos = baseText[2].split('$');
-
-	entrada = '$'+String(parseInt(infos[1])).slice(0,1)+'.'+String(parseInt(infos[1])).slice(1)+',00';
-	parcela = parseInt(infos[2]);
 
 	anoCarro = baseText[0].slice(0,4);
 	nomeCarro = baseText[0].slice(5);
 
-	$('#adStoryCanvas').addLayer({
-		
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: (1080/2), y: 1770,
-		fontSize: 80,
-		fontFamily: 'Montserrat',
-		text: '/MÊS',
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: ((1080/3)+50), y: 1630,
-		fontSize: 300,
-		fontFamily: 'Bebas Neue, cursive',
-		text: parcela,
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: ((1080/4)-80), y: 1560,
-		fontSize: 100,
-		fontFamily: 'Bebas Neue, cursive',
-		text: '$',
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: (1080/4), y: 1440,
-		fontSize: 100,
-		fontFamily: 'Bebas Neue, cursive',
-		text: entrada,
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#6d6c6a',
-		x: (1080/2.4), y: 1440,
-		fontSize: 200,
-		fontFamily: 'Bebas Neue, cursive',
-		text: '+',
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: (1080/4), y: 1350,
-		fontSize: 40,
-		fontFamily: 'Montserrat, cursive',
-		text: 'ENTRADA DE',
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: (1080/2), y: 250,
-		fontSize: 40,
-		fontFamily: 'Montserrat, cursive',
-		text: anoCarro,
-		maxWidth: 800
-	}).drawText({
-		layer: true,
-		index: 2,
-		fillStyle: '#F0AF39',
-		x: (1080/2), y: 320,
-		fontSize: 80,
-		fontFamily: 'Bebas Neue, cursive',
-		text: nomeCarro,
-		maxWidth: 800
-	}).addLayer({
-		type: 'image',
-		index:1,
-		source: selo,
-		x: (1080/1.4), y: 1350
-	}).addLayer({
-		type: 'image',
-		index:1,
-		source: 'StoryOverlayTop.png',
-		x: (1080/2), y: 250
-	}).addLayer({
-		type: 'image',
-		index:1,
-		source: 'StoryOverlayBottom.png',
-		x: (1080/2), y:1575
-	}).addLayer({
-		type: 'image',
-		name: 'storyImage',
-	  	source: storyImage,  
-	  	x: (1080/2), y: position,
-		scale:scale,
-		draggable: true,
-		index:0,
-	});
+	if (entryPrice && monthlyPrice) {
+		entrada = "$"+entryPrice
+		parcela = monthlyPrice
 
-	$('#adStoryCanvas').drawLayers();
+		$('#adStoryCanvas').addLayer({
+		
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/2), y: 1770,
+			fontSize: 80,
+			fontFamily: 'Montserrat',
+			text: '/MÊS',
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: ((1080/3)+50), y: 1630,
+			fontSize: 300,
+			fontFamily: 'Bebas Neue, cursive',
+			text: parcela,
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: ((1080/4)-80), y: 1560,
+			fontSize: 100,
+			fontFamily: 'Bebas Neue, cursive',
+			text: '$',
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/4), y: 1440,
+			fontSize: 100,
+			fontFamily: 'Bebas Neue, cursive',
+			text: entrada,
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#6d6c6a',
+			x: (1080/2.4), y: 1440,
+			fontSize: 200,
+			fontFamily: 'Bebas Neue, cursive',
+			text: '+',
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/4), y: 1350,
+			fontSize: 40,
+			fontFamily: 'Montserrat, cursive',
+			text: 'ENTRADA DE',
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/2), y: 250,
+			fontSize: 40,
+			fontFamily: 'Montserrat, cursive',
+			text: anoCarro,
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/2), y: 320,
+			fontSize: 80,
+			fontFamily: 'Bebas Neue, cursive',
+			text: nomeCarro,
+			maxWidth: 800
+		}).addLayer({
+			type: 'image',
+			index:1,
+			source: selo,
+			x: (1080/1.4), y: 1350
+		}).addLayer({
+			type: 'image',
+			index:1,
+			source: 'StoryOverlayTop.png',
+			x: (1080/2), y: 250
+		}).addLayer({
+			type: 'image',
+			index:1,
+			source: 'StoryOverlayBottom.png',
+			x: (1080/2), y:1575
+		}).addLayer({
+			type: 'image',
+			name: 'storyImage',
+			  source: storyImage,  
+			  x: (1080/2), y: position,
+			scale:scale,
+			draggable: true,
+			index:0,
+		});
+	
+		$('#adStoryCanvas').drawLayers();
+	} else if (singlePrice) {
+		cashPrice = singlePrice
+
+		$('#adStoryCanvas').addLayer({
+		
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/3)-30, y: 1650,
+			fontSize: 60,
+			fontFamily: 'Montserrat',
+			text: 'CASH PRICE',
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: ((1080/3)-30), y: 1540,
+			fontSize: 250,
+			fontFamily: 'Bebas Neue, cursive',
+			text: cashPrice,
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: ((1080/4)-170), y: 1500,
+			fontSize: 100,
+			fontFamily: 'Bebas Neue, cursive',
+			text: '$',
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/2), y: 250,
+			fontSize: 40,
+			fontFamily: 'Montserrat, cursive',
+			text: anoCarro,
+			maxWidth: 800
+		}).drawText({
+			layer: true,
+			index: 2,
+			fillStyle: '#F0AF39',
+			x: (1080/2), y: 320,
+			fontSize: 80,
+			fontFamily: 'Bebas Neue, cursive',
+			text: nomeCarro,
+			maxWidth: 800
+		}).addLayer({
+			type: 'image',
+			index:1,
+			source: selo,
+			x: (1080/1.4), y: 1350
+		}).addLayer({
+			type: 'image',
+			index:1,
+			source: 'StoryOverlayTop.png',
+			x: (1080/2), y: 250
+		}).addLayer({
+			type: 'image',
+			index:1,
+			source: 'StoryOverlayBottom.png',
+			x: (1080/2), y:1575
+		}).addLayer({
+			type: 'image',
+			name: 'storyImage',
+			  source: storyImage,  
+			  x: (1080/2), y: position,
+			scale:scale,
+			draggable: true,
+			index:0,
+		});
+	
+		$('#adStoryCanvas').drawLayers();
+	}
+
+	
 });
 
 
